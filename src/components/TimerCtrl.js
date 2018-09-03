a2bc95918e8c3324b907399101a9a766fd019b98
YukiMinamiHHNii <homu.homu.nii@gmail.com>
2018-09-02 20:29:32 -0500

    Fixes broken reference to timer component

--- src/components/TimerCtrl.js
+++ src/components/TimerCtrl.js
@@ -1,26 +1,26 @@
 import React from "react";
 
-class Timer extends React.Component {
-	
+class TimerCtrl extends React.Component {
 	render() {
 		return (
 			<div id="timer-ctrl">
 				<h2 id="timer-label">{this.props.type}</h2>
-				<h3 id="time-left">
-					{this.props.time}
-				</h3>
+				<h3 id="time-left">{this.props.time}</h3>
 				<div className="pad">
-					<button id="start_stop" value="start/stop" onClick={this.props.handler}>
+					<button
+						id="start_stop"
+						value="start/stop"
+						onClick={this.props.handler}
+					>
 						Start/Stop
 					</button>
 					<button id="reset" value="reset" onClick={this.props.reset}>
 						Reset
 					</button>
-					
 				</div>
 			</div>
 		);
 	}
 }
 
-export default Timer;
+export default TimerCtrl;
